70a9fe5c6334f309134d70de43f3cba06ff23767
pranjitlahkar <plahkar63@gmail.com>
2025-09-26 00:14:35 +0530

    postcss added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './app/**/*.{js,ts,jsx,tsx,mdx}',
-        './pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        '/app/**/*.{js,ts,jsx,tsx,mdx}',
+        '/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        '/src/components/**/*.{js,ts,jsx,tsx,mdx}',
         // Add the path to your Navbartwo.jsx file if it's elsewhere
-        './components/Navbartwo.jsx',
+        '/src/components/Navbartwo.jsx',
     ],
     // This is the important part that fixes your mobile menu
     safelist: [
